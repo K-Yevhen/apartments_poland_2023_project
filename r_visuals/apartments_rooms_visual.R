04ce74ee3630b0b232a59dbb8cacc12012ce75fe
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2024-12-11 15:11:30 +0100

    Number of Rooms R script and visualisation
    created data visualisation with ggplot tool : using a geom_bar to detect which rooms more suitable for building and buying

---
+++ r_visuals/apartments_rooms_visual.R
@@ -0,0 +1,21 @@
+---
+  title: 'Visualizing'
+---
+  
+# firstly need to install package to work with "ggplot2"
+install.packages("ggplot2")
+# loading library to make visuals
+library(ggplot2)
+library(scales)
+
+# importing data
+krk_apartments <- read.csv('apartments_krakow_6month_final.csv')
+
+ggplot(data=krk_apartments, aes(x = factor(rooms))) +
+  geom_bar(fill = "purple", color = "black", width = 0.7) +
+  labs(
+    title = "Apartments by Number of Rooms in Krakow",
+    x = "Number of Rooms",
+    y = "Number of Apartments"
+  ) +
+  theme_minimal()

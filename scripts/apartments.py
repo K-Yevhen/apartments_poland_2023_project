71fcde82eaf3efd2411c5fea60b8136606c158c2
Yevhen <112542705+K-Yevhen@users.noreply.github.com>
2024-12-17 15:09:15 +0100

    updated StreamLit script
    1. created advance chart visualisation with buildYear vs Price:
    - dropped N/A values and sorted in buildYear column
    - added average price for better understanding of year price summary
    - created new column "smoothed_price" with rolling function
    - builded Plotly Line Chart
    2. made short description

--- scripts/apartments.py
+++ scripts/apartments.py
@@ -1,8 +1,7 @@
 # importing libraries for future analysis
-
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import plotly.express as px
 
 # loading data to work with
 apartments_krakow = pd.read_csv("apartments_krakow_6month_final.csv")
@@ -12,7 +11,7 @@ print(apartments_krakow.head())
 
 # filtering data
 min_price = st.sidebar.slider(
-    "Minimum Price (PLN)",
+    "Minimum Price in Krakow 2024 (PLN)",
     min_value=int(apartments_krakow['price'].min()),
     max_value=int(apartments_krakow['price'].max()),
     value=int(apartments_krakow['price'].min()),
@@ -45,3 +44,36 @@ st.markdown(
     """
 )
 
+# dropping missing value (esp. in buildYear column) and sorting by buildYear
+apartments_krakow_v1 = apartments_krakow.dropna(subset=["buildYear", "price"])
+apartments_krakow_v1 = apartments_krakow_v1.sort_values(by="buildYear")
+
+# Grouping and calculating data
+average_price_by_year = apartments_krakow_v1.groupby("buildYear", as_index=False)["price"].mean()
+
+# Smoothing data using a rolling average, window = size of year
+average_price_by_year["smoothed_price"] = average_price_by_year["price"].rolling(window=5, min_periods=1).mean()
+
+# building Plotly Line Chart
+fig = px.line(
+    average_price_by_year,
+    x="buildYear",
+    y="smoothed_price",
+    title="Trend of Average Price Over Build Year",
+    labels={"buildYear": "Build Year", "smoothed_price": "Average Price (PLN)"},
+    line_shape="linear",
+    template="plotly_dark",
+)
+
+# Displaying the chart
+st.markdown("#### Relationship Between Build Year and Average Price")
+st.plotly_chart(fig)
+
+# Describing short explanation about difference by min and max buildYear
+st.markdown("""
+### Summary of Findings
+The chart shows that **older buildings** in Krakow tend to be **more expensive**.
+This is because they are often located **closer to the city center** and are known
+as **"Kamienice"** in Polish. These historic buildings are valued for their **prime locations**,
+**classic architecture**, and cultural significance, which makes them highly desirable despite their age.
+""")
